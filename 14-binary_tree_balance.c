3a59db3406e0b0de5242b3fc904094eaac9a0c61
Anthony <huggins9000211@gmail.com>
2020-03-11 14:53:39 -0400

    test

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,43 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Entry point
+ *
+ * @tree: tree
+ *
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_height_rec(tree));
+}
+
+/**
+ * binary_tree_height_rec - Entry point
+ *
+ * @tree: tree
+ *
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_height_rec(const binary_tree_t *tree)
+{
+	int l;
+	int r;
+
+	if (!tree)
+		return (-1);
+	l = binary_tree_height_rec(tree->left);
+	r = binary_tree_height_rec(tree->right);
+
+	if (l > r)
+		return (l + 1);
+	else
+		return (r + 1);
+}
+
+/**
  * binary_tree_balance - Entry point
  *
  * @tree: tree
